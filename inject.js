f3a46d203f4259422f0613cf742d56dee1763871
Luke Sheppard <luke@luke-sheppard.com>
2022-05-18 17:05:34 +0100

    Show percentage to current price

--- inject.js
+++ inject.js
@@ -63,43 +63,60 @@ async function populate() {
 
     results.forEach(async result => {
         const propId = result.id.replace('property-', '');
-        
+        const currentPrice = result.querySelector('.propertyCard-priceValue').innerHTML;
+
         const deliveryPointId = await getDeliveryPointId(propId);
 
         const url = apiUrl + deliveryPointId;
         const target = result.querySelector('.propertyCard-price');
 
-        const div = document.createElement('div');
-        div.className = "rm-sale-history";
-
         const response = await fetch(url);
         const data = await response.json();
-        let html = '';
 
         debug(`Found ${data.soldPropertyTransactionHistories.length} transactions for propId ${propId}, pointId ${deliveryPointId}`);
 
-        if (data.soldPropertyTransactionHistories.length === 0) {
-            html = "No sale history";
-        } else {
-            html = "<u><b>Sale History</b></u>";
-            html += "<table>";
-            
-            data.soldPropertyTransactionHistories.forEach(transaction => {
-                html += `
-                    <tr>
-                        <td>${transaction.year}</td>
-                        <td>${transaction.soldPrice}</td>
-                        <td>${transaction.percentageDifference}</td>
-                    </tr>
-                `;
-            });
-
-            html += "</table>";
-        }
-
-        div.innerHTML = html;
-        target.append(div);
+        target.append(buildDiv(data.soldPropertyTransactionHistories, currentPrice));
     });
 
     startInterval();
+}
+
+function buildDiv(transactions, currentPrice) {
+    let html = "No sale history";
+
+    const div = document.createElement('div');
+    div.className = "rm-sale-history";
+
+    if (transactions.length > 0) {
+        html = "<u><b>Sale History</b></u>";
+        html += "<table>";
+
+        const numericCurrentPrice = currentPrice.replace(/\D/g, '');
+        const numericLastSalePrice = transactions[0].soldPrice.replace(/\D/g, '')
+        const currentPercentageDifference = Math.round(numericCurrentPrice / numericLastSalePrice * 100);
+
+        html += `
+            <tr>
+                <td>Now</td>
+                <td>${currentPrice}</td>
+                <td>${(currentPercentageDifference > 0 ? '+' : '') + currentPercentageDifference}%</td>
+            </tr>
+        `;
+        
+        transactions.forEach(transaction => {
+            html += `
+                <tr>
+                    <td>${transaction.year}</td>
+                    <td>${transaction.soldPrice}</td>
+                    <td>${transaction.percentageDifference}</td>
+                </tr>
+            `;
+        });
+
+        html += "</table>";
+    }
+
+    div.innerHTML = html;
+
+    return div;
 }
